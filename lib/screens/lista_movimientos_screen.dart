4bf3f31f368c0e33172052e5b3437cac9f7d5a8b
GastonIvanHaagGomez <Gasel88@hotmail.com>
2024-11-22 10:33:45 -0300

    lista de movimiento mejorada visualmente

--- lib/screens/lista_movimientos_screen.dart
+++ lib/screens/lista_movimientos_screen.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:practico2labo4/screens/screens.dart';
-import 'package:practico2labo4/screens/visualizacion_movimiento_screen.dart'; // Importante para manejar el tiempo de espera y reintentos
+import 'package:practico2labo4/screens/visualizacion_movimiento_screen.dart';
 
 class ListaMovimientosScreen extends StatefulWidget {
   const ListaMovimientosScreen({super.key});
@@ -31,7 +31,6 @@ class _ListaMovimientosScreenState extends State<ListaMovimientosScreen> {
     super.dispose();
   }
 
-  // Función para hacer reintentos en la solicitud HTTP
   Future<void> fetchMovimientos(String url, {int retries = 3}) async {
     setState(() {
       isLoading = true;
@@ -45,17 +44,14 @@ class _ListaMovimientosScreenState extends State<ListaMovimientosScreen> {
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
 
-        // Verificar si el widget sigue montado antes de llamar a setState()
         if (!isDisposed) {
           setState(() {
-            movimientos.addAll(data[
-                'results']); // Agregar los nuevos movimientos a la lista existente
-            nextUrl = data['next']; // Establecer la URL de la siguiente página
+            movimientos.addAll(data['results']);
+            nextUrl = data['next'];
             isLoading = false;
           });
         }
 
-        // Si hay una URL para la siguiente página, carga los próximos movimientos
         if (nextUrl != null && !isDisposed) {
           fetchMovimientos(nextUrl!);
         }
@@ -64,18 +60,15 @@ class _ListaMovimientosScreenState extends State<ListaMovimientosScreen> {
       }
     } on TimeoutException catch (_) {
       if (retries > 0) {
-        // Si se produce un TimeoutException, intentar de nuevo
         fetchMovimientos(url, retries: retries - 1);
       } else {
         setState(() {
           isLoading = false;
         });
-        throw Exception(
-            'Tiempo de espera agotado. No se pudo conectar a la API.');
+        throw Exception('Tiempo de espera agotado. No se pudo conectar a la API.');
       }
     } on http.ClientException catch (_) {
       if (retries > 0) {
-        // Intentar nuevamente si la solicitud fue rechazada
         fetchMovimientos(url, retries: retries - 1);
       } else {
         setState(() {
@@ -100,37 +93,60 @@ class _ListaMovimientosScreenState extends State<ListaMovimientosScreen> {
               itemBuilder: (context, index) {
                 final movimiento = movimientos[index];
 
-                // Generamos una URL de imagen para el movimiento actual (puedes mejorar la lógica para personalizar la imagen)
-                int pokemonId = index +
-                    1; // Para probar, generamos un ID de Pokémon basado en el índice (del 1 al 100)
+                int pokemonId = index + 1;
                 String imageUrl = generatePokemonImageUrl(pokemonId);
 
-                return ListTile(
-                  title: Text(movimiento['name'].toUpperCase()),
-                  trailing: Image.network(
-                    imageUrl,
-                    width: 50, // Ajusta el tamaño de la imagen
-                    height: 50,
-                    fit: BoxFit.cover,
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => VisualizacionMovimientoScreen(
-                          url: movimiento['url'],
-                        ),
+                return Container(
+                  margin: const EdgeInsets.symmetric(
+                      horizontal: 15, vertical: 10), // Márgenes alrededor
+                  padding: const EdgeInsets.all(10), // Relleno interno
+                  decoration: BoxDecoration(
+                    color: Colors.yellow.shade100, // Fondo amarillo claro
+                    borderRadius: BorderRadius.circular(15), // Bordes redondeados
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        blurRadius: 8,
+                        spreadRadius: 2,
+                        offset: const Offset(4, 4), // Sombra desplazada
                       ),
-                    );
-                  },
+                    ],
+                  ),
+                  child: ListTile(
+                    leading: Image.network(
+                      imageUrl,
+                      width: 50,
+                      height: 50,
+                      fit: BoxFit.cover,
+                    ),
+                    title: Text(
+                      movimiento['name'].toUpperCase(),
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black87,
+                      ),
+                    ),
+                    trailing: const Icon(Icons.arrow_forward_ios, color: Colors.black54),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => VisualizacionMovimientoScreen(
+                            url: movimiento['url'],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 );
               },
             ),
     );
   }
 
-  // Función para generar una URL de imagen de Pokémon para el movimiento (suponiendo que cada movimiento tiene una imagen asociada)
   String generatePokemonImageUrl(int id) {
     return 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$id.png';
   }
 }
+
