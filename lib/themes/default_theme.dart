b8b76bfa35cf65e3445530cb233765723b77ef05
Evelin <evelinrpaumgertner@hotmail.com>
2024-11-26 12:07:19 -0300

    color light-dark

--- lib/themes/default_theme.dart
+++ lib/themes/default_theme.dart
@@ -4,12 +4,13 @@ class DefaultTheme {
   static const Color primaryDark = Color(0xFFFF0000); // Rojo Pokémon
   static const Color secondary = Color(0xFF2D2D2D); // Gris oscuro
   static const Color formElementsColor =
-      Color.fromARGB(255, 255, 223, 0); // Amarillo Pokémon (como Pikachu)
+      Color.fromARGB(255, 45, 115, 208); // Amarillo Pokémon (como Pikachu)
   static final Color primaryHover = primaryDark.withOpacity(0.8);
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-    primaryColor: primaryDark,
+    primaryColor: const Color.fromARGB(255, 56, 121, 225),
     scaffoldBackgroundColor: Colors.white,
+  
     appBarTheme: const AppBarTheme(
       backgroundColor: primaryDark,
       foregroundColor: Colors.white,
@@ -23,7 +24,7 @@ class DefaultTheme {
     ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: TextButton.styleFrom(
-        backgroundColor: const Color.fromARGB(255, 255, 251, 0),
+        backgroundColor: const Color.fromARGB(255, 54, 129, 190),
         foregroundColor: Colors.white,
         padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
