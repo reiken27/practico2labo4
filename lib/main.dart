f3a07b628a988f6b687021fcbe3f975ebdbb3944
GastonIvanHaagGomez <gasel88@hotmail.com>
2024-11-26 21:16:08 -0300

    grif y cambios  disenio y colores  lista movimientos

--- lib/main.dart
+++ lib/main.dart
@@ -68,4 +68,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-//prueba
--- lib/screens/visualizacion_movimiento_screen.dart
+++ lib/screens/visualizacion_movimiento_screen.dart
@@ -41,240 +41,175 @@ class _VisualizacionMovimientoScreenState
     return Scaffold(
       appBar: AppBar(
         title: const Text('Detalles del Movimiento'),
+        centerTitle: true,
       ),
       body: movimiento == null
           ? const Center(child: CircularProgressIndicator())
-          : Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: SingleChildScrollView(
-                child: Container(
-                  padding: const EdgeInsets.all(16.0),
-                  decoration: BoxDecoration(
-                    color: Colors.yellow.shade100,
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.2),
-                        blurRadius: 10,
-                        spreadRadius: 2,
-                        offset: const Offset(4, 4),
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // Título principal
-                      Center(
-                        child: Text(
-                          'Nombre del Movimiento:\n${movimiento?['name']?.toUpperCase()}',
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            fontSize: 28,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black87,
-                          ),
+          : Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.blueAccent, Colors.lightBlue],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SingleChildScrollView(
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.9),
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          blurRadius: 8,
+                          spreadRadius: 2,
+                          offset: const Offset(4, 4),
                         ),
-                      ),
-                      const SizedBox(height: 24),
-
-                      // Detalles destacados
-                      const Text(
-                        'DETALLES DESTACADOS',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.black54,
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-
-                      if (movimiento?['power'] != null)
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 4.0),
-                          child: Row(
-                            children: [
-                              const Text(
-                                'Poder: ',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                '${movimiento?['power']}',
-                                style: const TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.black87,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      if (movimiento?['accuracy'] != null)
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 4.0),
-                          child: Row(
-                            children: [
-                              const Text(
-                                'Precisión: ',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                '${movimiento?['accuracy']}',
-                                style: const TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.black87,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      if (movimiento?['pp'] != null)
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 4.0),
-                          child: Row(
-                            children: [
-                              const Text(
-                                'PP: ',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                '${movimiento?['pp']}',
-                                style: const TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.black87,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      if (movimiento?['effect_entries'] != null &&
-                          (movimiento?['effect_entries'] as List).isNotEmpty)
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 4.0),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text(
-                                'Efecto: ',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Expanded(
-                                child: Text(
-                                  '${movimiento?['effect_entries'][0]['effect']}',
-                                  style: const TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.black87,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-
-                      const SizedBox(height: 16),
-
-                      // Comentarios y favoritos
-                      const Text(
-                        'COMENTARIOS Y FAVORITOS',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.black54,
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-
-                      // Campo de comentarios
-                      TextFormField(
-                        controller: _controller,
-                        decoration: const InputDecoration(
-                          labelText: 'Ingresa un comentario',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-
-                      // Switch para marcar como favorito
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            'Es favorito:',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          Switch(
-                            value: isFavorite,
-                            onChanged: (value) {
-                              setState(() {
-                                isFavorite = value;
-                              });
-                            },
-                          ),
-                        ],
-                      ),
-
-                      const SizedBox(height: 16),
-
-                      // Sección de lista completa
-                      const Text(
-                        'LISTA COMPLETA DE DETALLES',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.black54,
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-
-                      ListView.builder(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemCount: movimiento?.length ?? 0,
-                        itemBuilder: (context, index) {
-                          final key = movimiento!.keys.elementAt(index);
-                          final value = movimiento![key].toString();
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 4.0),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  '${key.toUpperCase()}: ',
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Text(
-                                    value,
-                                    style: const TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.black87,
-                                    ),
-                                  ),
-                                ),
-                              ],
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Título principal
+                        Center(
+                          child: Text(
+                            movimiento?['name']?.toUpperCase() ?? '',
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.indigo,
                             ),
-                          );
-                        },
-                      ),
-                    ],
+                          ),
+                        ),
+                        const SizedBox(height: 24),
+
+                        // Detalles destacados
+                        const Text(
+                          'Detalles Destacados',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.teal,
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+
+                        if (movimiento?['power'] != null)
+                          DetailRow(
+                            label: 'Poder',
+                            value: '${movimiento?['power']}',
+                          ),
+                        if (movimiento?['accuracy'] != null)
+                          DetailRow(
+                            label: 'Precisión',
+                            value: '${movimiento?['accuracy']}',
+                          ),
+                        if (movimiento?['pp'] != null)
+                          DetailRow(
+                            label: 'PP',
+                            value: '${movimiento?['pp']}',
+                          ),
+                        if (movimiento?['effect_entries'] != null &&
+                            (movimiento?['effect_entries'] as List).isNotEmpty)
+                          DetailRow(
+                            label: 'Efecto',
+                            value:
+                                '${movimiento?['effect_entries'][0]['effect']}',
+                          ),
+
+                        const SizedBox(height: 24),
+
+                        // Comentarios y favoritos
+                        const Text(
+                          'Comentarios y Favoritos',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 150, 0, 0),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+
+                        // Campo de comentarios
+                        TextFormField(
+                          controller: _controller,
+                          decoration: InputDecoration(
+                            labelText: 'Ingresa un comentario',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+
+                        // Switch para marcar como favorito
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              'Es favorito:',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Switch(
+                              value: isFavorite,
+                              onChanged: (value) {
+                                setState(() {
+                                  isFavorite = value;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
+    );
+  }
+}
+
+// Widget para fila de detalle
+class DetailRow extends StatelessWidget {
+  final String label;
+  final String value;
+
+  const DetailRow({required this.label, required this.value, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '$label: ',
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: Colors.indigo,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              value,
+              style: const TextStyle(
+                fontSize: 20,
+                color: Colors.black87,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
