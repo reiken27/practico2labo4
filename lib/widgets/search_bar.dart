23e5e23e642e9c740f1d5edfa57dd326fc2ad603
Emiliano Correa <emiliano.correa.amigo@gmail.com>
2024-12-20 11:15:30 -0300

    search_bar

---
+++ lib/widgets/search_bar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class PokemonSearchBar extends StatelessWidget {
+  final TextEditingController searchController;
+  final VoidCallback onClearSearch;
+  final ValueChanged<String> onSearchChanged;
+
+  const PokemonSearchBar({
+    Key? key,
+    required this.searchController,
+    required this.onClearSearch,
+    required this.onSearchChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        controller: searchController,
+        decoration: InputDecoration(
+          hintText: 'Buscar Pokemon...',
+          prefixIcon: const Icon(Icons.search, color: Colors.blue),
+          suffixIcon: IconButton(
+            icon: const Icon(Icons.clear, color: Colors.blue),
+            onPressed: onClearSearch,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: const BorderSide(color: Colors.blue),
+          ),
+        ),
+        onChanged: onSearchChanged,
+      ),
+    );
+  }
+}
